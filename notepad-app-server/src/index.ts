baa18e29e47bca95acbf16e0e90d1fb56ae3b84d
arthurjohnson17 <arthurjohnson17@gmail.com>
2025-07-01 22:34:56 +0100

    Added api calls

--- notepad-app-server/src/index.ts
+++ notepad-app-server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from './generated/prisma'
 
 const app = express();
 const prisma = new PrismaClient();
@@ -9,8 +9,78 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
-    const notes = await prisma.note.findMany();
-    res.json(notes);
+    try{
+        const notes = await prisma.note.findMany();
+        res.json(notes);
+    } catch (error)
+    {
+        res.status(500).send("Something went wrong: faield to find");
+    }
+});
+
+app.post("/api/notes", async (req, res) => {
+    const { title, content } = req.body;
+
+    if(!title || !content){
+        res.status(400).send("title and content fields required");
+        return;
+    }
+
+    try{
+        const note = await prisma.note.create({
+            data: { title, content},
+        });
+        res.json(note);
+    } catch (error)
+    {
+        res.status(500).send("Something went wrong: faield to create");
+    }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+    const {title, content} = req.body;
+    const id = parseInt(req.params.id);
+
+    if(!id || isNaN(id)) {
+        res.status(400).send("ID must be a valid number");
+        return;
+    }
+
+    if(!title || !content){
+        res.status(400).send("title and content fields required");
+        return;
+    }
+
+    try {
+        const updateNote = await prisma.note.update({
+            where: { id },
+            data: {title, content},
+        });
+        res.json(updateNote);
+    }  catch (error)
+    {
+        res.status(500).send("Something went wrong: failed to update");
+    }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if(!id || isNaN(id)) {
+        res.status(400).send("ID must be a valid number");
+        return;
+    }
+
+    try {
+        await prisma.note.delete({
+            where: { id },
+        });
+        res.status(204).send();
+    }  catch (error)
+    {
+        res.status(500).send("Something went wrong: failed to delete");
+    }
+
 });
 
 app.listen(5000, ()=> {
--- notepad-app-ui/src/App.tsx
+++ notepad-app-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css"
 
 type Note = {
@@ -10,28 +10,7 @@ type Note = {
 const App = () => {
   const [notes, setNotes] = useState<
     Note[]
-  >([
-    {
-      id: 1,
-      title: "note title 1",
-      content: "content 1"
-    },
-    {
-      id: 2,
-      title: "note title 2",
-      content: "content 2"
-    },
-    {
-      id: 3,
-      title: "note title 3",
-      content: "content 3"
-    },
-    {
-      id: 4,
-      title: "note title 4",
-      content: "content 4"
-    },
-  ]);
+  >([]);
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -39,29 +18,59 @@ const App = () => {
   const [selectedNote, setSelectedNote] =
     useState<Note | null>(null);
 
+  useEffect(()=> {
+    const fetchNotes = async ()=>{
+      try{
+        const response = 
+          await fetch("http://localhost:5000/api/notes");
+        
+        const notes: Note[] = await response.json();
+        setNotes(notes);
+        } catch (e) {
+          console.log(e);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+
   const handleNoteClick = (note:Note) => {
     setSelectedNote(note);
     setTitle(note.title);
     setContent(note.content);
   };
 
-  const handleAddNote = (
+  const handleAddNote = async (
     event:React.FormEvent
   ) => {
     event.preventDefault();
 
-    const newNote: Note = {
-      id: notes.length + 1,
-      title: title,
-      content: content
-    }
+    try{
+      const response = await fetch(
+        "http://localhost:5000/api/notes",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            content
+          })
+        }
+      );
 
-    setNotes([newNote, ...notes]);
-    setTitle("");
-    setContent("");
+      const newNote = await response.json();
+
+      setNotes([newNote, ...notes]);
+      setTitle("");
+      setContent("");
+    } catch (e) {
+          console.log(e);
+      }
   };
 
-  const handleUpdateNote = (
+  const handleUpdateNote = async (
     event: React.FormEvent
   ) => {
     event.preventDefault();
@@ -70,22 +79,36 @@ const App = () => {
       return;
     }
 
-    const updatedNote: Note = {
-      id: selectedNote.id,
-      title: title,
-      content: content,
-    }
+    try{
+      const response = await fetch(
+        `http://localhost:5000/api/notes/${selectedNote.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            content
+          })
+        }
+      );
 
-    const updatedNoteslist = notes.map((note) =>
-      note.id === selectedNote.id
-        ? updatedNote
-        : note
-    )
+      const updatedNote = await response.json();
 
-    setNotes(updatedNoteslist)
-    setTitle("")
-    setContent("")
-    setSelectedNote(null)
+      const updatedNoteslist = notes.map((note) =>
+        note.id === selectedNote.id
+          ? updatedNote
+          : note
+      );
+
+      setNotes(updatedNoteslist)
+      setTitle("")
+      setContent("")
+      setSelectedNote(null)
+    } catch (e) {
+          console.log(e);
+      }
   };
 
   const handleCancel = () => {
@@ -94,21 +117,32 @@ const App = () => {
     setSelectedNote(null)
   };
 
-  const deleteNote = (
+  const deleteNote = async (
       event :React.MouseEvent,
       noteId: number
     ) => {
       event.stopPropagation();
 
-      const updatedNotes = notes.filter(
-        (note)=> note.id != noteId
-      )
+      try {
+        await fetch(
+          `http://localhost:5000/api/notes/${noteId}`,
+          {
+            method: "Delete",
+          }
+        );
+        const updatedNotes = notes.filter(
+          (note)=> note.id != noteId
+        );
 
-      if(selectedNote && selectedNote.id === noteId){
-        handleCancel();
+        if(selectedNote && selectedNote.id === noteId){
+          handleCancel();
+        }
+
+        setNotes(updatedNotes);
+        
+      } catch (e) {
+        console.log(e);
       }
-
-      setNotes(updatedNotes);
   };
 
   return(<div className="app-container">
